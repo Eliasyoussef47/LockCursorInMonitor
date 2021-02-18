831decbaff23aef82e5eddb686344ca0e97bf85e
Elias youssef <d-s-a00@hotmail.com>
2021-02-18 15:00:51 +0100

    Add project files.

---
+++ LockCursorInMonitor/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Gma.System.MouseKeyHook;
+using System.Windows;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace LockCursorInMonitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //TODO: Bug: if you press ctrl and then press the title bar of a window the cursor clipt resets
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public int X;
+            public int Y;
+        };
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(ref Win32Point pt);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        static extern bool ClipCursor(RECT rcClip);
+
+        public bool Locked = false;
+
+        private IKeyboardMouseEvents m_GlobalHook;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GlobalHookKeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.LControlKey || e.KeyCode == Keys.RControlKey)
+            {
+                if (!Locked)
+                {
+                    System.Drawing.Point MousePoint = GetMousePosition();
+                    RECT bounds = Screen.GetWorkingArea(MousePoint);
+                    ClipCursor(bounds);
+                    Locked = true;
+                }
+            }
+        }
+
+        private void GlobalHookKeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.LControlKey || e.KeyCode == Keys.RControlKey)
+            {
+                if (Locked)
+                {
+                    ClipCursor(null);
+                    Locked = false;
+                }
+            }
+        }
+
+        public static System.Drawing.Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+
+            return new System.Drawing.Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+        private void ActivatedSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ModernWpf.Controls.ToggleSwitch activatedSwitch = (ModernWpf.Controls.ToggleSwitch)sender;
+            if (activatedSwitch.IsOn)
+            {
+                m_GlobalHook = Hook.GlobalEvents();
+
+                m_GlobalHook.KeyDown += GlobalHookKeyDown;
+                m_GlobalHook.KeyUp += GlobalHookKeyUp;
+            }
+            else
+            {
+                m_GlobalHook.KeyDown -= GlobalHookKeyDown;
+                m_GlobalHook.KeyUp -= GlobalHookKeyUp;
+
+                m_GlobalHook.Dispose();
+            }
+        }
+    }
+}
---
+++ LockCursorInMonitor/RECT.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace LockCursorInMonitor
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public class RECT
+    {
+        #region Variables.
+        /// <summary>
+        /// Left position of the rectangle.
+        /// </summary>
+        public int Left;
+        /// <summary>
+        /// Top position of the rectangle.
+        /// </summary>
+        public int Top;
+        /// <summary>
+        /// Right position of the rectangle.
+        /// </summary>
+        public int Right;
+        /// <summary>
+        /// Bottom position of the rectangle.
+        /// </summary>
+        public int Bottom;
+        #endregion
+
+        #region Operators.
+        /// <summary>
+        /// Operator to convert a RECT to Drawing.Rectangle.
+        /// </summary>
+        /// <param name="rect">Rectangle to convert.</param>
+        /// <returns>A Drawing.Rectangle</returns>
+        public static implicit operator Rectangle(RECT rect)
+        {
+            return Rectangle.FromLTRB(rect.Left, rect.Top, rect.Right, rect.Bottom);
+        }
+
+        /// <summary>
+        /// Operator to convert Drawing.Rectangle to a RECT.
+        /// </summary>
+        /// <param name="rect">Rectangle to convert.</param>
+        /// <returns>RECT rectangle.</returns>
+        public static implicit operator RECT(Rectangle rect)
+        {
+            return new RECT(rect.Left, rect.Top, rect.Right, rect.Bottom);
+        }
+        #endregion
+
+        #region Constructor.
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="left">Horizontal position.</param>
+        /// <param name="top">Vertical position.</param>
+        /// <param name="right">Right most side.</param>
+        /// <param name="bottom">Bottom most side.</param>
+        public RECT(int left, int top, int right, int bottom)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+
+        public RECT(System.Windows.Rect rect)
+        {
+            Left = (int)rect.Left;
+            Top = (int)rect.Top;
+            Right = (int)rect.Right;
+            Bottom = (int)rect.Bottom;
+        }
+        #endregion
+    }
+}
