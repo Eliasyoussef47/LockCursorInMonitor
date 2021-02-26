95a5c9190b17012a770b30cf9c53230c747daba0
Elias youssef <d-s-a00@hotmail.com>
2021-02-26 20:50:24 +0100

    Added the ability to save and load settings. Added slightly edited icons. Added a setup project. Added a system tray icon. Resolves #5

--- LockCursorInMonitor/MainWindow.xaml.cs
+++ LockCursorInMonitor/MainWindow.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using Gma.System.MouseKeyHook;
 using System.Windows;
-using System.Windows.Forms;
-using System.Runtime.InteropServices;
-using System.Diagnostics;
-using System.ComponentModel;
-using System;
+using Forms = System.Windows.Forms;
 using LockCursorInMonitor.Interop;
+using LockCursorInMonitor.Configurations;
+using System.IO;
+using System.Reflection;
+using System;
+using System.ComponentModel;
 
 namespace LockCursorInMonitor
 {
@@ -21,9 +22,23 @@ namespace LockCursorInMonitor
             InitializeComponent();
         }
 
+        private void CursorLockingWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ApplyConfigurations();
+        }
+
+        /// <summary>
+        /// Applies saved settings to the UI so that the UI represents the saved settings of the user.
+        /// </summary>
+        public void ApplyConfigurations()
+        {
+            AppConfigs appConfigs = Configs.ConfigsTools.GetConfigs<AppConfigs>();
+            ActivatedSwitch.IsOn = appConfigs.Activated;
+        }
+
         private void GlobalHookCtrlDown(object sender, System.Windows.Forms.KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.LControlKey || e.KeyCode == Keys.RControlKey)
+            if (e.KeyCode == Forms.Keys.LControlKey || e.KeyCode == Forms.Keys.RControlKey)
             {
                 if (!CursorLock.Locked)
                 {
@@ -34,7 +49,7 @@ namespace LockCursorInMonitor
 
         private void GlobalHookCtrlUp(object sender, System.Windows.Forms.KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.LControlKey || e.KeyCode == Keys.RControlKey)
+            if (e.KeyCode == Forms.Keys.LControlKey || e.KeyCode == Forms.Keys.RControlKey)
             {
                 if (CursorLock.Locked)
                 {
@@ -52,7 +67,7 @@ namespace LockCursorInMonitor
             }
         }
 
-        private void GlobalHookFocusChanged(object sender, MouseEventArgs e)
+        private void GlobalHookFocusChanged(object sender, Forms.MouseEventArgs e)
         {
             if (Native.GetKeyState(VirtualKeyStates.VK_CONTROL) < 0)
             {
@@ -62,6 +77,7 @@ namespace LockCursorInMonitor
 
         private void ActivatedSwitch_Toggled(object sender, RoutedEventArgs e)
         {
+            AppConfigs appConfigs = Configs.ConfigsTools.GetConfigs<AppConfigs>();
             ModernWpf.Controls.ToggleSwitch activatedSwitch = (ModernWpf.Controls.ToggleSwitch)sender;
             if (activatedSwitch.IsOn)
             {
@@ -75,6 +91,8 @@ namespace LockCursorInMonitor
                 m_GlobalHook.MouseDownExt += GlobalHookFocusChanged;
                 m_GlobalHook.MouseDragStarted += GlobalHookFocusChanged;
                 m_GlobalHook.MouseDragStartedExt += GlobalHookFocusChanged;
+
+                appConfigs.Activated = true;
             }
             else
             {
@@ -88,7 +106,19 @@ namespace LockCursorInMonitor
                 m_GlobalHook.MouseDragStartedExt -= GlobalHookFocusChanged;
 
                 m_GlobalHook.Dispose();
+
+                appConfigs.Activated = false;
             }
+            appConfigs.Save();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // Setting cancel to true will cancel the close request so the application is not closed
+            e.Cancel = true;
+            this.Hide();
+
+            base.OnClosing(e);
         }
     }
 }
