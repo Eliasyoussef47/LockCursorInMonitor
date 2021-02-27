a04d363ba999eb0ab0378d95f2c6852e200745b6
Elias youssef <d-s-a00@hotmail.com>
2021-02-27 02:02:59 +0100

    Added Configs from NuGet

--- LockCursorInMonitor/MainWindow.xaml.cs
+++ LockCursorInMonitor/MainWindow.xaml.cs
@@ -114,7 +114,7 @@ namespace LockCursorInMonitor
 
         protected override void OnClosing(CancelEventArgs e)
         {
-            // Setting cancel to true will cancel the close request so the application is not closed
+            // Setting cancel to true will cancel the close request so the application is not closed.
             e.Cancel = true;
             this.Hide();
 
